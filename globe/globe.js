c9881e997f552b52aef87287bdd2e89c56119610
Shawn Stedman <sstedman@commonsensemedia.org>
2014-08-22 17:10:11 -0700

    new 5000 city map data, line color tweak.

--- globe/globe.js
+++ globe/globe.js
@@ -19,7 +19,8 @@ DAT.Globe = function(container, opts) {
   var colorFn = opts.colorFn || function(x) {
     var c = new THREE.Color();
     // c.setHSL( ( 0.6 - ( x * 0.5 ) ), 1.0, 0.5 );
-    c.setRGB(0.25 + x * 0.75, 0.83 + x * 0.17, 0.25 + x * 0.75);
+    // green
+    c.setRGB(0.5 + x * 0.5, 0.83 + x * 0.17, 0.25 + x * 0.75);
     console.log("x = " + x);
     return c;
   };
